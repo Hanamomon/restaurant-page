827a92a0d9804c2905a252bae956b45e67ccf272
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2025-10-29 22:22:20 -0700

    feat: add the tab-switching logic

--- src/index.js
+++ src/index.js
@@ -2,8 +2,28 @@ import loadHome from "./home.js";
 import loadContact from "./contact.js";
 import loadMenu from "./menu.js";
 
+function clearContent() {
+    const contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "";
+}
+
+const homeBtn = document.getElementById("homeBtn");
+const menuBtn = document.getElementById("menuBtn");
+const contactBtn = document.getElementById("contactBtn");
+
 document.addEventListener("DOMContentLoaded", loadHome());
 
-document.addEventListener("DOMContentLoaded", loadContact());
+homeBtn.addEventListener("click", () => {
+    clearContent();
+    loadHome();
+});
 
-document.addEventListener("DOMContentLoaded", loadMenu());
+menuBtn.addEventListener("click", () => {
+    clearContent();
+    loadMenu();
+});
+
+contactBtn.addEventListener("click", () => {
+    clearContent();
+    loadContact();
+});
