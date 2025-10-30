32c8c915dcdc3bebc4e1621705859d448992a6fa
Hanamomon <145081281+Hanamomon@users.noreply.github.com>
2025-10-29 18:34:04 -0700

    feat: add home page tab structure

---
+++ src/home.js
@@ -0,0 +1,36 @@
+export default function () {
+    const contentDiv = document.getElementById("content");
+    const headline = document.createElement("h1");
+
+    const restauDiv = document.createElement("div");
+    const restauInfo = document.createElement("p");
+
+    const hoursDiv = document.createElement("div");
+    const hoursHead = document.createElement("h2");
+    const hoursWeekday = document.createElement("p");
+    const hoursWeekend = document.createElement("p");
+
+    const locationDiv = document.createElement("div");
+    const locationHead = document.createElement("h2");
+    const locationText = document.createElement("p");
+
+    headline.textContent = "Mimo's Proteins";
+    headline.setAttribute("id", "headline");
+
+    restauInfo.textContent = "Don't want to bother with the time-consuming surveillance and counting of nutrients, yet want to maintain a healthy diet? Mimo's Proteins is your answer! You will find your haven between succulent treats crafted with care for your body and knowledgeable servers that are at your disposal for excellent recommendations. Nothing beats the quality of our meals coupled with helping you make an informed choice. Your wellness is our priority!";
+    restauDiv.setAttribute("class", "info-container");
+    restauDiv.append(restauInfo);
+
+    hoursHead.textContent = "Hours of Operation";
+    hoursWeekday.textContent = "Monday - Saturday: 9AM - 9PM";
+    hoursWeekend.textContent = "Friday - Sunday: 10AM - 6PM";
+    hoursDiv.setAttribute("class", "info-container");
+    hoursDiv.append(hoursHead, hoursWeekday, hoursWeekend);
+
+    locationHead.textContent = "Location";
+    locationText.textContent = "906 Lougheed Hwy, Edmonton";
+    locationDiv.setAttribute("class", "info-container");
+    locationDiv.append(locationHead, locationText);
+
+    contentDiv.append(headline, restauDiv, hoursDiv, locationDiv);
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import loadHome from "./home.js";
+
+document.addEventListener("DOMContentLoaded", loadHome())
